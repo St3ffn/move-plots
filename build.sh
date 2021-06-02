fa1070cb85e73b23f92c6d078905ff16a07e414d
st3ffn <funk.up.up@gmail.com>
2021-06-02 16:30:06 +0200

    add build script and go mod

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname "$(realpath -s "$0")")
+
+# git commit id
+GIT_COMMIT=$(git rev-list -1 HEAD)
+# most recent tag
+GIT_VERSION_TAG=$(git describe --tags --abbrev=0)
+
+go build -v -o move-plots -ldflags="-X 'main.GitVersion=$GIT_VERSION_TAG'" "$SCRIPT_PATH/main.go"
+env GOOS=linux GOARCH=amd64 go build -v -o move-plots-linux-amd64 -ldflags="-X 'main.GitVersion=$GIT_VERSION_TAG'" "$SCRIPT_PATH/main.go"
+env GOOS=darwin GOARCH=amd64 go build -v -o move-plots-darwin-amd64 -ldflags="-X 'main.GitVersion=$GIT_VERSION_TAG'" "$SCRIPT_PATH/main.go"
+env GOOS=linux GOARCH=arm64 go build -v -o move-plots-linux-arm64 -ldflags="-X 'main.GitVersion=$GIT_VERSION_TAG'" "$SCRIPT_PATH/main.go"
---
+++ go.mod
@@ -0,0 +1,8 @@
+module move-plots
+
+go 1.16
+
+require (
+	github.com/St3ffn/plots-left v0.3.0 // indirect
+	github.com/urfave/cli/v2 v2.3.0 // indirect
+)

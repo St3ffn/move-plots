b7f1640ed57e58451cb9a1efb2f68052b1bb6354
st3ffn <funk.up.up@gmail.com>
2021-06-03 13:28:07 +0200

    remove verbose mode for build script

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ GIT_COMMIT=$(git rev-list -1 HEAD)
 # most recent tag
 GIT_VERSION_TAG=$(git describe --tags --abbrev=0)
 
-go build -v -o move-plots -ldflags="-X 'main.GitVersion=$GIT_VERSION_TAG'" "$SCRIPT_PATH/main.go"
-env GOOS=linux GOARCH=amd64 go build -v -o move-plots-linux-amd64 -ldflags="-X 'main.GitVersion=$GIT_VERSION_TAG'" "$SCRIPT_PATH/main.go"
-env GOOS=darwin GOARCH=amd64 go build -v -o move-plots-darwin-amd64 -ldflags="-X 'main.GitVersion=$GIT_VERSION_TAG'" "$SCRIPT_PATH/main.go"
-env GOOS=linux GOARCH=arm64 go build -v -o move-plots-linux-arm64 -ldflags="-X 'main.GitVersion=$GIT_VERSION_TAG'" "$SCRIPT_PATH/main.go"
+go build -o move-plots -ldflags="-X 'main.GitVersion=$GIT_VERSION_TAG'" "$SCRIPT_PATH/main.go"
+env GOOS=linux GOARCH=amd64 go build -o move-plots-linux-amd64 -ldflags="-X 'main.GitVersion=$GIT_VERSION_TAG'" "$SCRIPT_PATH/main.go"
+env GOOS=darwin GOARCH=amd64 go build -o move-plots-darwin-amd64 -ldflags="-X 'main.GitVersion=$GIT_VERSION_TAG'" "$SCRIPT_PATH/main.go"
+env GOOS=linux GOARCH=arm64 go build -o move-plots-linux-arm64 -ldflags="-X 'main.GitVersion=$GIT_VERSION_TAG'" "$SCRIPT_PATH/main.go"

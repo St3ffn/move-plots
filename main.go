7b32540253e7ca4ea22cc1090bd4ad343ef8bbfb
st3ffn <funk.up.up@gmail.com>
2021-06-04 08:05:25 +0200

    add main

--- main.go
+++ main.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"fmt"
-	"github.com/St3ffn/plots-left/pkg/disk"
 	"io"
 	"move-plots/internal/cli"
+	"move-plots/internal/logging"
+	"move-plots/internal/plot"
 	"os"
 )
 
 var (
-	GitVersion           = "0.0.0"
-	stderr     io.Writer = os.Stderr
-	stdout     io.Writer = os.Stdout
+	GitVersion                   = "0.0.0"
+	stdout     io.Writer         = os.Stdout
+	stderr     io.Writer         = os.Stderr
+	loggers    logging.LogFacade = logging.NewLoggers(stdout, stderr)
 )
 
 func main() {
 	if err := run(); err != nil {
-		_, _ = fmt.Fprintf(stderr, "%s\n", err)
+		loggers.E().Println(err.Error())
 		os.Exit(1)
 	}
 }
@@ -30,9 +32,47 @@ func run() error {
 	if ctx.Done {
 		return nil
 	}
-	_, err = disk.NewDisk(ctx.Source)
+
+	if ctx.Verbose {
+		loggers = logging.NewVerboseLoggers(stdout, stderr)
+	}
+
+	plots, err := plot.FindPlots(ctx.Source)
 	if err != nil {
 		return err
 	}
+	if len(plots) == 0 {
+		loggers.I().Printf("no plots found in %s. nothing to do\n", ctx.Source)
+		return nil
+	}
+
+	loggers.I().Printf("found %d plots in %s\n", len(plots), ctx.Source)
+
+	for _, plotFilename := range plots {
+		sourcePlot := plot.AbsoluteFilename(ctx.Source, plotFilename)
+
+		loggers.V().Println("try to move", sourcePlot)
+
+		targetDisk, err := plot.FindDisk(ctx.Reserved, ctx.Targets)
+		if err != nil {
+			return err
+		}
+		if targetDisk == nil {
+			return fmt.Errorf("can not move plot %s. no disk space left", sourcePlot)
+		}
+
+		loggers.V().Printf("found %s with available capacity for %d plots\n", targetDisk.Path, targetDisk.PlotsLeft())
+
+		targetPlot := plot.AbsoluteFilename(targetDisk.Path, plotFilename)
+
+		loggers.V().Printf("try to move %s to %s\n", sourcePlot, targetPlot)
+
+		if err := plot.MovePlot(ctx.Source, plotFilename, targetDisk); err != nil {
+			return err
+		}
+
+		loggers.I().Printf("moved %s to %s\n", sourcePlot, targetPlot)
+	}
+
 	return nil
 }
